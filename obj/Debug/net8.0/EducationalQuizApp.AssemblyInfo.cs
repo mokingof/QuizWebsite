9c00d6568bb7f5b2f54c9cc85048fc5242badd1a
Abdill <dmohammed@hotmail.co.uk>
2024-04-28 19:26:51 +0100

    more styling changes

--- obj/Debug/net8.0/EducationalQuizApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/EducationalQuizApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EducationalQuizApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b306c2ebb3da65a286b38218d433c8e19184d062")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26d94a59f2282612f800bf577f4225316450f2d2")]
 [assembly: System.Reflection.AssemblyProductAttribute("EducationalQuizApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EducationalQuizApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
