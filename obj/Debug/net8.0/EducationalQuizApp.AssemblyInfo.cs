328884b8ce0d1d638589099f522b084ee01cb173
Abdill <dmohammed@hotmail.co.uk>
2024-05-01 23:00:05 +0100

    Styling the quiz. I think its perfect. So im going to move onto the Submit and Next Question button

--- obj/Debug/net8.0/EducationalQuizApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/EducationalQuizApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EducationalQuizApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1661579bc62eb5cc9a8d3081d3878e2df67e6322")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1020539164e7985f21aa55bb3130f2140bd1adc")]
 [assembly: System.Reflection.AssemblyProductAttribute("EducationalQuizApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EducationalQuizApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
