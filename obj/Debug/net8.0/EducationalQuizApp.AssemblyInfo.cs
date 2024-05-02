1ecf14977ce6c2e60fa1d3376cae90dcef51e647
Abdill <dmohammed@hotmail.co.uk>
2024-05-02 19:48:57 +0100

    Buttons.

--- obj/Debug/net8.0/EducationalQuizApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/EducationalQuizApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EducationalQuizApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+328884b8ce0d1d638589099f522b084ee01cb173")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c0914ea9e7ca8787a10284c0c7ef9a119226b10")]
 [assembly: System.Reflection.AssemblyProductAttribute("EducationalQuizApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EducationalQuizApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
